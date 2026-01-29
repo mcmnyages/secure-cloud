edb7adf2f713ed843bc63f399f3232304c44c339
McMnyages <morachasilas@gmail.com>
2026-01-29 07:59:17 +0300

    refactor: remove unused import of useQuery from useSendVerification hook

--- client/src/hooks/email/useSendVerification.ts
+++ client/src/hooks/email/useSendVerification.ts
@@ -1,4 +1,4 @@
-import { useMutation,useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { emailService } from "../../api/services/emailService";
 import { toast } from "sonner";
 
