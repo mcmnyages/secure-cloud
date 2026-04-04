3879a4f375b78f6ee1e66edc6c2a203d187efc98
McMnyages <morachasilas@gmail.com>
2026-04-04 23:15:46 +0300

    fix: remove deprecated option from tsconfig and update vite config for path resolution

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path/win32'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
     tailwindcss()],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
