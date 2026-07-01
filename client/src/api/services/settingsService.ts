7bbbaab4108f8ebac9c60a0506ba4ebfd42daeb4
McMnyages <morachasilas@gmail.com>
2026-07-01 22:10:24 +0300

    feat: implement getUserSettings method in settingsService for user settings retrieval

--- client/src/api/services/settingsService.ts
+++ client/src/api/services/settingsService.ts
@@ -1,6 +1,8 @@
-import axios from '../axios'
-import type { UserSettings } from '@/types/settingsTypes'
+import api from '../axios'
 
 export const settingsService={
-   
+   getUserSettings: async ()=>{
+    const response = await api.get('/settings/user');
+    return response;
+   }
 }
